6263c73ee86174dcf06db93489fa88f3bb463fbf
Alberto97 <albertop2197@gmail.com>
2018-08-08 22:12:25 +0200

    Accents: Specify current SDK version

--- Default/Android.mk
+++ Default/Android.mk
@@ -5,6 +5,7 @@ LOCAL_RRO_THEME := DefaultAccent
 LOCAL_PACKAGE_NAME := DefaultAccent
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_CERTIFICATE := platform
+LOCAL_SDK_VERSION := current
 
 include $(BUILD_RRO_PACKAGE)
 
--- Pixel/Android.mk
+++ Pixel/Android.mk
@@ -5,5 +5,6 @@ LOCAL_RRO_THEME := PixelAccent
 LOCAL_PACKAGE_NAME := PixelAccent
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_CERTIFICATE := platform
+LOCAL_SDK_VERSION := current
 
 include $(BUILD_RRO_PACKAGE)
